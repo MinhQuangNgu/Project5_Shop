e8274c162348809745a48a8a828b6af84d8fd31f
MinhQuangNgu <quangnm160393@gmail.com>
2022-12-22 15:59:46 +0700

    change

--- src/foodDetail/Detail.js
+++ src/foodDetail/Detail.js
@@ -265,45 +265,58 @@ const Detail = () => {
                                 className="detail_body_infor_content_1"
                             ></div>
                         )}
-                        <div className="detail_body_infor_content_2">
-                            <div className="detail_body_infor_content_title">
-                                <div>
-                                    <i
-                                        style={{
-                                            marginRight: "0.5rem",
-                                            marginTop: "0.5rem",
-                                        }}
-                                        className="fa-solid fa-comment"
-                                    ></i>
-                                    <h2>Đánh Giá</h2>
+                        {type === 2 && (
+                            <div className="detail_body_infor_content_2">
+                                <div className="detail_body_infor_content_title">
+                                    <div>
+                                        <div>
+                                            <i
+                                                style={{
+                                                    marginRight: "0.5rem",
+                                                    marginTop: "0.5rem",
+                                                }}
+                                                className="fa-solid fa-comment"
+                                            ></i>
+                                            <h2>Đánh Giá</h2>
+                                        </div>
+                                        <div>
+                                            {starArr.map((_, index) => (
+                                                <i
+                                                    onMouseOver={() => {
+                                                        setHover(index + 1);
+                                                    }}
+                                                    onMouseLeave={() => {
+                                                        setHover(null);
+                                                    }}
+                                                    onClick={() => {
+                                                        setStar(index + 1);
+                                                    }}
+                                                    key={index + "star"}
+                                                    className={
+                                                        hover
+                                                            ? hover > index
+                                                                ? "fa-solid fa-star"
+                                                                : "fa-regular fa-star"
+                                                            : star > index
+                                                            ? "fa-solid fa-star"
+                                                            : "fa-regular fa-star"
+                                                    }
+                                                ></i>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button>Gửi</button>
+                                    </div>
                                 </div>
-                                <div>
-                                    {starArr.map((_, index) => (
-                                        <i
-                                            onMouseOver={() => {
-                                                setHover(index + 1);
-                                            }}
-                                            onMouseLeave={() => {
-                                                setHover(null);
-                                            }}
-                                            onClick={() => {
-                                                setStar(index + 1);
-                                            }}
-                                            key={index + "star"}
-                                            className={
-                                                hover
-                                                    ? hover > index
-                                                        ? "fa-solid fa-star"
-                                                        : "fa-regular fa-star"
-                                                    : star > index
-                                                    ? "fa-solid fa-star"
-                                                    : "fa-regular fa-star"
-                                            }
-                                        ></i>
-                                    ))}
+                                <div className="detail_body_infor_content_input_wrap">
+                                    <div
+                                        contentEditable="true"
+                                        className="detail_body_infor_content_input"
+                                    ></div>
                                 </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
