e7d91bbc27f83219b724a7e6ec3e37f37bae7dd1
MinhQuangNgu <quangnm160393@gmail.com>
2022-12-26 08:37:04 +0700

    change

--- src/card/ReplyCard.js
+++ src/card/ReplyCard.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import ReplyCard from "./ReplyCard";
 import ReplyInput from "./ReplyInput";
 import "./style.scss";
-const CommentCard = () => {
+const ReplyCard = () => {
     const [reply, setReply] = useState(false);
 
+    const [likes, setLikes] = useState(0);
+
     return (
-        <div className="commentCard">
+        <div className="commentCard reply">
             <div className="commentCard_img">
                 <img
                     src="https://res.cloudinary.com/sttruyen/image/upload/v1670845412/Sttruyenxyz/tsmietvqzskvkakoe4fk.jpg"
@@ -23,7 +24,9 @@ const CommentCard = () => {
                     </div>
                 </div>
                 <div className="commentCard_navbar">
-                    <div className="commentCard_navbar_items">0 Thích</div>
+                    <div className="commentCard_navbar_items">
+                        {likes} Thích
+                    </div>
                     <div
                         onClick={() => {
                             setReply(!reply);
@@ -38,15 +41,12 @@ const CommentCard = () => {
                 </div>
                 {reply && (
                     <div className="commentCard_replyInput">
-                        <ReplyInput name="Quang Khon" />
+                        <ReplyInput name="Quang Ngu" />
                     </div>
                 )}
-                <div className="commentCard_reply">
-                    <ReplyCard />
-                </div>
             </div>
         </div>
     );
 };
 
-export default CommentCard;
+export default ReplyCard;
