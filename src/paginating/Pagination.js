d83dff69f4d8c2b1ab952164f6a38976e4bc9363
MinhQuangNgu <quangnm160393@gmail.com>
2023-01-03 12:45:02 +0700

    change

--- src/paginating/Pagination.js
+++ src/paginating/Pagination.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./style.scss";
 const Pagination = () => {
-    return <div className="page">Pagination</div>;
+    return (
+        <div className="paging">
+            <div className="paging_items">
+                <i className="fa-solid fa-angle-left"></i>
+            </div>
+            <div className="paging_items active">1</div>
+            <div className="paging_items">2</div>
+            <div className="paging_items">
+                <i className="fa-solid fa-angle-right"></i>
+            </div>
+        </div>
+    );
 };
 
 export default Pagination;
